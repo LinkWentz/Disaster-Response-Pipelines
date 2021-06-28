8b58ec6771a3109d7487e8f0a7e2c55225ba4339
Lincoln Wentz <linkwentz@gmail.com>
2021-06-27 19:08:57 -0500

    Refactor process_data.py to reflect template

--- data/process_data.py
+++ data/process_data.py
@@ -1,11 +1,8 @@
+import sys
 import numpy as np
 import pandas as pd
 import sqlite3 as sql
 
-messages = pd.read_csv('messages.csv')
-categories = pd.read_csv('categories.csv')
-
-# Dummy-ify categories.
 def condense_category_string(category_string,
                              cat_sep = ';',
                              val_sep = '-'):
@@ -32,27 +29,68 @@ def condense_category_string(category_string,
     
     return dense_cats_string
 
-categories['categories'] = list(map(condense_category_string, 
-                                    categories['categories']))
-dummy_categories = categories.categories.str.get_dummies(sep = ';')
-categories = pd.concat([categories['id'], dummy_categories], axis = 1)
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    
+    messages = messages.merge(categories, on = 'id')
+    
+    return messages
 
-# Merge tables
-messages = messages.merge(categories, on = 'id')
+def clean_data(df):
+    # Dummy-ify categories.
+    df['categories'] = list(map(condense_category_string, 
+                                df['categories']))
+    dummy_categories = df.categories.str.get_dummies(sep = ';')
+    df = pd.concat([df[4:], dummy_categories], axis = 1)
+    
+    # Remove duplicates.
+    # Sort duplicates by amount of categories to ensure that the duplicate with the
+    # most categorizations is the one kept.
+    category_matrix = np.matrix(df[list(df.columns)[5:]])
+    print(category_matrix)
+    cat_count = category_matrix.sum(axis = 1)
+    df['cat_count'] = cat_count
+    df = df.sort_values('cat_count', ascending = False)
 
-# Remove duplicates.
-# Sort duplicates by amount of categories to ensure that the duplicate with the
-# most categorizations is the one kept.
-category_matrix = np.matrix(messages[list(messages.columns)[4:]])
-cat_count = category_matrix.sum(axis = 1)
-messages['cat_count'] = cat_count
-messages = messages.sort_values('cat_count', ascending = False)
-# Some duplicate messages had different IDs, making this a better approach.
-messages.drop_duplicates(subset = ['message'], inplace = True)
+    # Some duplicate messages had different IDs, making this a better approach.
+    df.drop_duplicates(subset = ['message'], inplace = True)
 
-messages = messages.drop('cat_count', axis = 1).sort_values('id')
+    df = df.drop('cat_count', axis = 1).sort_values('id')
+    
+    return df
 
-# Write to SQL database.
-conn = sql.connect('disaster_messages.db')
-messages.to_sql('categorized_messages', conn)
-conn.close()
+def save_data(df, database_filename):
+    conn = sql.connect(database_filename)
+    df.to_sql('categorized_messages', conn)
+    conn.close()
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'\
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
