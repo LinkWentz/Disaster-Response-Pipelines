ad86712e61e7a59c67cdeed417ce89fdad4aaf36
Lincoln Wentz <linkwentz@gmail.com>
2021-07-23 02:32:09 -0500

    Refactor run.py

--- app/run.py
+++ app/run.py
@@ -1,40 +1,56 @@
+# Imports.
+import joblib
 import json
-import re
 import numpy as np
 import pandas as pd
 import plotly
+import re
 import sqlite3 as sql
-
+# nltk imports.
+from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
-from flask import Flask
-from flask import render_template, request, jsonify
+stopwords = set(stopwords.words('english'))
+# Flask imports.
+from flask import Flask, render_template, request, jsonify
+# Plotly imports.
 from plotly.graph_objs import Bar
-import joblib
-from sqlalchemy import create_engine
-
 
 app = Flask(__name__)
 
-def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
+def tokenize(string):
+    """Returns an iterable of clean tokens made from the provided string.
+    """
+    # Normalize string.
+    string = string.lower()
+    substitutions = [
+        ('[\']', '',), # Remove apostrophes.
+        ('[^a-zA-Z0-9]', ' ',), # Convert non-alphanumeric chars to spaces.
+        (' {2,}', ' ',), # Convert multiple spaces to single space.
+        ('^ ', '',), # Remove leading space.
+        (' $', '',)  # Remove trailing space.
+    ]
+    for pattern, replacement in substitutions:
+        string = re.sub(pattern, replacement, string)
+    # Tokenize string.
+    tokens = string.split(' ')
+    tokens = [word for word in tokens if word not in stopwords]
 
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
+    return tokens
 
-    return clean_tokens
+def title(string):
+    """Convert provided string to title case.
+    """
+    string = re.sub('_', ' ', string) # Replace every underscore with a space.
+    string = string.title()
+    
+    return string
 
-def title(text):
-    text = re.sub('_', ' ', text)
-    text = text.title()
-    return text
-
-def get_count(column_name):
-    count = df.groupby(column_name).count().loc[1]['message']
+def get_count(category):
+    """Get count of messages which fit into the provided category."""
+    count = df.groupby(category).count().loc[1]['message']
+    
     return count
 
 conn = sql.connect('../data/DisasterResponse.db')
@@ -46,60 +62,59 @@ most_common_words = pd.read_sql_query('SELECT * FROM most_common_words', conn)
 
 conn.close()
 # Get category labels and convert them to title case
-category_labels = [title(category) for category in df.columns[5:]]
-category_counts = [get_count(category) for category in df.columns[5:]]
+cat_names = [title(category) for category in df.columns[5:]]
+messages_per_category = [get_count(category) for category in df.columns[5:]]
 # load model
 model = joblib.load("../models/classifier.pkl")
 
-
-# Index webpage displays cool visuals and receives user input text for model
+# Index webpage displays cool visuals and receives user input text for model.
 @app.route('/')
 @app.route('/index')
 def index():
-    # Save user input in query
+    # Save user input in query.
     query = request.args.get('query', '')
-    # Get the predictions for each category
-    category_values = model.predict([query])[0]
-    # Associate the predictions with the category labels
-    category_list = list(zip(category_labels,
-                             category_counts,
-                             category_values))
-    category_list = list(map(lambda x : {'name':x[0],'count':x[1],'value':x[2]},
-                             category_list))
-    # Reshape list into table
+    # Get the predictions for each category.
+    cat_values = model.predict([query])[0]
+    # Associate the predictions with the category labels.
+    category_tuples = list(zip(cat_names, messages_per_category, cat_values))
+    category_dicts = list(map(lambda x : {'name':x[0],'count':x[1],'value':x[2]},
+                              category_tuples))
+    # Reshape list into table.
     category_table = []
     cats_per_row = 3
     for i in np.arange(0, 36, cats_per_row):
-        category_table.append(category_list[i:i+cats_per_row])
-    # Extract data needed for visuals
+        category_table.append(category_dicts[i:i+cats_per_row])
+
+    # Extract data needed for visuals.
     cat_count_counts = df.groupby('cat_count').count()['message']
     cat_count_labels = list(np.arange(0, 10))
-
-    print(most_common_words)
-    # Create visuals
+    
+    most_common_word_counts = most_common_words['sum']
+    most_common_word_labels = [title(word) for word in most_common_words['index']]
+    # Create visuals.
     graphs = [
         {
             'data': [
                 Bar(
-                    x=cat_count_labels,
-                    y=cat_count_counts
+                    x = cat_count_labels,
+                    y = cat_count_counts
                 )
             ],
 
             'layout': {
-                'title': 'Messages By Amount of Categories',
+                'title': 'Messages by Category Count',
                 'yaxis': {
                     'title': ''
                 },
-                'width':500,
-                'height':500
+                'width':  500,
+                'height': 500
             }
         },
         {
             'data': [
                 Bar(
-                    x=most_common_words['sum'],
-                    y=most_common_words['index'],
+                    x = most_common_word_counts,
+                    y = most_common_word_labels,
                     orientation='h'
                 )
             ],
@@ -107,20 +122,19 @@ def index():
             'layout': {
                 'title': 'Most Common Words',
                 'yaxis': {
-                    'title': '',
                     'categoryorder':'total ascending'
                 },
-                'width':500,
-                'height':500
+                'width':  500,
+                'height': 500
             }
         }
     ]
     
-    # encode plotly graphs in JSON
+    # Encode plotly graphs in JSON.
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     
-    # render web page with plotly graphs
+    # Render web page with plotly graphs.
     return render_template('master.html', ids=ids, query=query, 
                            graphJSON=graphJSON, category_table=category_table)
 
